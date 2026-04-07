6573a4d525c948bdf4bf9f77990e4882512ae81c
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-07 19:26:35 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/faizunspringecs98/FaiznSpring98ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faizunspringecs98;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaiznSpring98ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
